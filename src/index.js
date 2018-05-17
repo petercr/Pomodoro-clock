f4501c4670e0aead787a93ff9af357ff16f12869
Peter <petecruckshank@gmail.com>
2018-05-17 19:14:00 -0400

    got the main timer working!  keeps running after ;-)

--- src/index.js
+++ src/index.js
@@ -1,85 +1,78 @@
-
-
-// timer amount times 60 to convert for seconds 
-let timer = 25 * 60; 
+// timer amount times 60 to convert for seconds
+let timer = 25 * 60;
 let breakTimer = 5 * 60;
-
-
 
 // IEEF fucntion to call on DOM load
 (function() {
-    // initialize the drop down menu from Materialize CSS
-    const alarmSounds = document.querySelectorAll("select");
-    const alarmSelection = M.FormSelect.init(alarmSounds);
+  // initialize the drop down menu from Materialize CSS
+  const alarmSounds = document.querySelectorAll("select");
+  const alarmSelection = M.FormSelect.init(alarmSounds);
 
-    // add event listener for the start button
-    document.querySelector("#startButton").addEventListener("click", () => {
-        startTheClock();
-    })
-    
-    
+  // add event listener for the start button
+  document.querySelector("#startButton").addEventListener("click", () => {
+    let getTimeArr = document.getElementById("timer").innerText.split(":");
+    let duration = parseInt(getTimeArr[0] * 60);
+    duration += parseInt(getTimeArr[1]);
+    console.log(duration);
+
+    startTheClock(duration);
+  });
 })();
 
+function startTheClock(duration) {
+  let start = Date.now(),
+    diff,
+    minutes,
+    seconds;
 
-function startTheClock(seconds) {
+  const display = document.querySelector("#timer");
 
-    // get Time from the main timer on the page
-    let getTimeArr = document.getElementById("timer").innerText.split(':');
+  function timer() {
+    // const display = document.querySelector("#timer");
+    // get the number of seconds that have elapsed since
+    // startTimer() was called
+    diff = duration - (((Date.now() - start) / 1000) | 0);
 
-    // set variables for minutes and seconds in milliseconds
-    const minutesToMils = getTimeArr[0] * 60 * 1000;
-    const secondsToMils = getTimeArr[1] * 1000;
+    // does the same job as parseInt truncates the float
+    minutes = (diff / 60) | 0;
+    seconds = (diff % 60) | 0;
 
-    // var to hold the added value of minutes and seconds
-    let totalMils = minutesToMils + secondsToMils;
-    console.log(totalMils);
-    
-    // waits the total minutes and seconds then calls alarm()
-    let theClock = window.setTimeout(alarm, totalMils);
-    if (theClock != null) {
-        console.log("timer started");
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (diff <= 0) {
+      // add one second so that the count down starts at the full duration
+      // example 05:00 not 04:59
+      start = Date.now() + 1000;
     }
+  }
+  // we don't want to wait a full second before the timer starts
+  timer();
+  let mainTimer = setInterval(timer, 1000);
+  let alarmTimer = setTimeout(alarm, duration * 1000);
 
-    // add event listener for the stop button
-    document.querySelector("#stopButton").addEventListener("click", function(theClock){
-        stopTheClock(theClock);
+  
+  // add event listener for the stop button
+  document
+    .querySelector("#stopButton")
+    .addEventListener("click", function(theClock) {
+      stopTheClock(theClock);
     });
 
-    // if seconds != zero then count down seconds to zero
-    if (secondsToMils !== 0){
-
-    }
-
-    
-
+  
 }
 
-// Function that is activated when alarm goes off 
+// Function that is activated when alarm goes off
 function alarm() {
-    alert("ALARM");
+  alert("ALARM");
 }
 
-// Function to handle the seconds clock
-function secondsTicker() {
-    let clock = document.getElementById("timer").textContent;
-    let timeArr = clock.split(":");
-    let seconds = timerArr[1];
-
-    // if there's more than one second left minus 1 second
-    if (seconds > 1){
-        seconds--;
-    } 
-    // if no more seconds then exit function 
-    else {
-        return;
-    }
-    
-    clock.textContent = timerArr[0] + ":" + seconds;
-
-}
 
 // Function to stop the clock
 function stopTheClock(clock) {
-    window.clearTimeout(clock);
-    console.log("timer stopped");
+  window.clearTimeout(alarmTimer);
+  window.clearInterval(mainTimer);
+  console.log("timer stopped");
 }
