a11c4bce9bf20fe3d40d2b79914ad24370a9e6f0
Peter <petecruckshank@gmail.com>
2018-12-07 17:42:07 -0500

    init base code from first parcel try

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: {
+        minutes: "25",
+        seconds: "00"
+      },
+      selection: "Session"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Pomodoro Clock</h1>
         </header>
+        <div>
+          <p>{this.state.selection}</p>
+          <p>
+            {this.state.timer.minutes}:{this.state.timer.seconds}
+          </p>
+        </div>
       </div>
     );
   }
