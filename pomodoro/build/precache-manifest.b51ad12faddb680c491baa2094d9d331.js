eaced78094bb05e126d97571a349737c698f9e3f
Peter <petecruckshank@gmail.com>
2018-12-27 11:03:29 -0500

    react build files plus the test script from freeCodeCamp

---
+++ pomodoro/build/precache-manifest.b51ad12faddb680c491baa2094d9d331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8cf6736b6b3beebc09c7",
+    "url": "/static/js/main.8cf6736b.chunk.js"
+  },
+  {
+    "revision": "c6a6f0ddd46869624f15",
+    "url": "/static/js/1.c6a6f0dd.chunk.js"
+  },
+  {
+    "revision": "8cf6736b6b3beebc09c7",
+    "url": "/static/css/main.7b1c2fe2.chunk.css"
+  },
+  {
+    "revision": "c6a6f0ddd46869624f15",
+    "url": "/static/css/1.6483d9d3.chunk.css"
+  },
+  {
+    "revision": "d3ffc0ba1ecd926a9c5d9910e38fe253",
+    "url": "/index.html"
+  }
+];
