689fd5c93fe74df3476f6173f167a759bd93fe66
Peter <petecruckshank@gmail.com>
2018-12-27 11:29:40 -0500

    added test script to componentDidMount( ) It now loads the test script

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -204,6 +204,16 @@ class App extends Component {
     setTimeout(this.startTheClock, 1500);
   }
 
+  componentDidMount() {
+    const script = document.createElement("script");
+
+    script.src =
+      "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     const initSession = this.state.initSessionLength;
     const initBreak = this.state.initBreakLength;
